54745368e5c8b658983b432d2b71d6ff23b2175a
samsilversteinn <sesilversteinn@gmail.com>
2024-02-02 14:28:25 -0500

    milestone #2

--- js/gallery.js
+++ js/gallery.js
@@ -35,72 +35,104 @@ function animate() {
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
 	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
+	//with a new image from your images array which is loaded
 	//from the JSON string
-	console.log('swap photo');
-}
-
-// Counter for the mImages array
-var mCurrentIndex = 0;
-
-// XMLHttpRequest variable
-var mRequest = new XMLHttpRequest();
-
-// Array holding GalleryImage objects (see below).
-var mImages = [];
-
-// Holds the retrived JSON information
-var mJson;
-
-// URL for the JSON to load by default
-// Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'images.json';
-
-
-//You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
-//@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
-function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
-		galleryImage.img = e.target;
-		mImages.push(galleryImage);
-	}
-}
-
-$(document).ready( function() {
-	
+	$("#photo").attr("src", mImages[mCurrentIndex].img);
+	mCurrentIndex += 1;
+	mCurrentIndex %= mImages.length;
+  }
+  
+  
+  // Counter for the mImages array
+  var mCurrentIndex = 0;
+  
+  
+  // XMLHttpRequest variable
+  var mRequest = new XMLHttpRequest();
+  
+  
+  // Array holding GalleryImage objects (see below).
+  var mImages = [];
+  
+  
+  // Holds the retrieved JSON information
+  var mJson;
+  
+  
+  // URL for the JSON to load by default
+  // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
+  var mUrl = "images.json";
+  
+  
+  //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
+  //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
+  function makeGalleryImageOnloadCallback(galleryImage) {
+	return function (e) {
+	  galleryImage.img = e.target;
+	  mImages.push(galleryImage);
+	};
+  }
+  
+  
+  $(document).ready(function () {
 	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
-	fetchJSON()
-});
-
-window.addEventListener('load', function() {
-	
-	console.log('window loaded');
-
-}, false);
-
-function GalleryImage() {
-	//implement me as an object to holdhart the following data about an image:
-	//1. location where photo was taken
-	let location 
-	//2. description of photo
-	let description 
-	//3. the date when the photo was taken
-	let date 
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
-	let img 
-}
-
-function fetchJSON() {
-	mRequest.open('GET', mUrl)
-	mRequest.send()
-	mRequest.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-			mJson = JSON.parse(mRequest.responseText);
-			console.log("This is working!");
-		} else {
-			console.log("We connected to the server, but it returned an error.");
-		}
-		
+	$(".details").eq(0).hide();
+	fetchJSON();
+  });
+  
+  
+  window.addEventListener(
+	"load",
+	function () {
+	  console.log("window loaded");
+	},
+	false
+  );
+  
+  
+  class GalleryImage {
+	constructor(location, description, date, img) {
+	  //implement me as an object to hold the following data about an image:
+	  //1. location where photo was taken
+	  this.location = location;
+	  //2. description of photo
+	  this.description = description;
+	  //3. the date when the photo was taken
+	  this.date = date;
+	  //4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+	  this.img = img;
 	}
-}
+  }
+  
+  
+  function fetchJSON() {
+	mRequest.open("GET", mUrl);
+	mRequest.send();
+	mRequest.onreadystatechange = function () {
+	  if (mRequest.readyState == 4 && mRequest.status == 200) {
+		mJson = JSON.parse(mRequest.responseText);
+		iterateJSON()
+		console.log("hi");
+	  } else {
+		console.log("We connected to the server, but it returned an error.");
+	  }
+	};
+  }
+  
+  
+  function iterateJSON() {
+	for (let image of mJson.images) {
+	  mImages.push(
+		new GalleryImage(
+		  image.imgLocation,
+		  image.description,
+		  image.date,
+		  image.imgPath
+		)
+	  );
+	}
+  }
+  
+  
+  
+  
